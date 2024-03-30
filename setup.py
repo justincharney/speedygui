3e2472d09177b703d0412e8d7a053c59dba1d329
Justin Charney <justin.charney@gmail.com>
2024-03-30 18:19:30 -0400

    updated package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="speedygui",
-    version="0.1.2",
+    version="0.1.3",
     author="Justin Charney",
     author_email="justin.charney@gmail.com",
     description="A simple template for creating GUI PyTorch medical image segmentation applications",
